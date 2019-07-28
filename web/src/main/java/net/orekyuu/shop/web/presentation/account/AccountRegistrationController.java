db9f9dd6ed0f065682bdd3aa7bb930db1f4ee9db
orekyuu <orekyuu@gmail.com>
2019-07-28 17:46:11 +0900

    メール送信の準備

--- web/src/main/java/net/orekyuu/shop/web/presentation/account/AccountRegistrationController.java
+++ web/src/main/java/net/orekyuu/shop/web/presentation/account/AccountRegistrationController.java
@@ -2,7 +2,9 @@ package net.orekyuu.shop.web.presentation.account;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("accounts")
@@ -11,5 +13,10 @@ public class AccountRegistrationController {
     @GetMapping("registration")
     public String showRegistration() {
         return "accounts/registration";
+    }
+
+    @PostMapping("registration/mail")
+    public String sendRegistrationMail(@RequestParam("mail") String mail) {
+        return "accounts/registration_mail";
     }
 }
