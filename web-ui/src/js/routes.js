5964b9ea63e5de35c2ce1b6f67929ad9963b40c6
orekyuu <orekyuu@gmail.com>
2019-09-01 22:24:31 +0900

    ログイン画面をVueにする

--- web-ui/src/js/routes.js
+++ web-ui/src/js/routes.js
@@ -3,9 +3,11 @@ import Home from './page/Home'
 import SendMail from './page/accounts/registration/SendMail'
 import AccountRegistration from './page/accounts/registration/AccountRegistration'
 import InvalidTokenError from './page/accounts/registration/InvalidTokenError'
+import Login from './page/Login'
 
 const routes = [
   { path: '/home', component: Home },
+  { path: '/login', component: Login },
   { path: '/accounts/registration', component: AccountRegistration },
   { path: '/accounts/registration/mail', component: SendMail },
   { path: '/accounts/registration/invalid_token', component: InvalidTokenError }
